17926652e1605df33894852c7e727628d2237635
leon-panops <leon@panops.com.br>
2024-01-30 13:32:03 -0300

    - Ajustando models

--- api/serializers.py
+++ api/serializers.py
@@ -2,7 +2,25 @@ from django.contrib.auth.models import User
 from rest_framework import serializers
 
 
-class UserSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = User
-        fields = ('id', 'username', 'first_name', 'last_name', 'email')
+class UserSerializer(serializers.Serializer):
+    id = serializers.IntegerField(required=False)
+    username = serializers.CharField()
+    password = serializers.CharField(write_only=True, required=False)
+    first_name = serializers.CharField()
+    last_name = serializers.CharField()
+    is_staff = serializers.BooleanField()
+    is_active = serializers.BooleanField()
+    cpf = serializers.CharField()
+    address = serializers.JSONField()
+    phone1 = serializers.CharField()
+    phone2 = serializers.CharField()
+    phone_other1 = serializers.CharField()
+    phone_other2 = serializers.CharField()
+    whats = serializers.CharField()
+    date_nasc = serializers.DateField()
+
+    def create(self, validated_data):
+        pass
+
+    def update(self, instance, validated_data):
+        pass
